547541cc5a8cf1b02393091c99194f01c8c0508a
saha122 <kinshuksaha4@gmail.com>
2025-04-28 19:54:36 +0530

    header change

--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -5,11 +5,13 @@ const Header = () => {
     
   return (
     <div className='header'>
-      <div className="header-contain">
+      <div className='headerBg'>
+        <div className="header-contain">
         <h2>Feast Faster, Dine Smarter</h2>
         <p>Welcome to our one-stop destination for all your dining needs! Whether you're craving a gourmet feast or a quick bite, our food ordering and restaurant seat booking website has you covered.</p>
         <button className="table"> <a href='#booktable'>Book Table</a></button>
 
+        </div>
       </div>
     </div>
   )
