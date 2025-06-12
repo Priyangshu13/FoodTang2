65ea7298f3afb2c108aaa028ffc2eec882741c22
saha122 <kinshuksaha4@gmail.com>
2025-06-13 02:38:37 +0530

    changes

--- frontend/src/pages/BookingForm/BookingForm.jsx
+++ frontend/src/pages/BookingForm/BookingForm.jsx
@@ -6,35 +6,39 @@ const BookingForm = () => {
     <div className='Main_bookingcontainer'>
         <div className="booking-container">
             <div className="form-section">
-                <h1 className="logo">FOODTANG</h1>
-                <div className="form-group">
-                <label>CITY</label>
-                <select>
-                    <option>---- SELECT YOUR CITY ----</option>
-                        <option value="Kolkata">Kolkata</option>
-                        <option value="Durgapur">Durgapur</option>
-                        <option value="Asansol">Asansol</option>
-                </select>
-                </div>
-                <div className="form-group">
-                    <label>PERSON</label>
-                    <select>
-                        <option>---- SELECT NUMBER OF PERSON ----</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4+</option>
-                    </select>
-                </div>
-                <div className="form-group">
-                    <label>CHOOSE DATE</label>
-                    <input type="date" />
-                </div>
-                <div className="form-group">
-                    <label>CHOOSE TIME</label>
-                    <input type="time" />
-                </div>
+             <div className='Formlabels'>
+                            <h1 className="logo">FOODTANG</h1>
+                            <div className="form-group">
+                            <label>CITY</label>
+                            <select>
+                                <option>---- SELECT YOUR CITY ----</option>
+                                    <option value="Kolkata">Kolkata</option>
+                                    <option value="Durgapur">Durgapur</option>
+                                    <option value="Asansol">Asansol</option>
+                            </select>
+                            </div>
+                            <div className="form-group">
+                                <label>PERSON</label>
+                                <select>
+                                    <option>---- SELECT NUMBER OF PERSON ----</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4+</option>
+                                </select>
+                            </div>
+                            <div className="form-group">
+                                <label>CHOOSE DATE</label>
+                                <input type="date" />
+                            </div>
+                            <div className="form-group">
+                                <label>CHOOSE TIME</label>
+                                <input type="time" />
+                            </div>
+                        </div>
+        <div className='bookbtn'>
                 <button className="book-button">BOOK TABLE</button>
+        </div>
             </div>
             <div className="image-section">
                 <p className="image-placeholder">Image here</p>
